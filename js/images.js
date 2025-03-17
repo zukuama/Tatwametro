7f65bc3bcfc737d4c455d369f404a5f1ca0ab875
zukuama <73803653+zukuama@users.noreply.github.com>
2025-03-17 08:49:03 -0300

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,13 +1,13 @@
 function loadTatwaImage(imageName) {
     const imgElement = document.getElementById("tatwa-image");
-    let imagePath = `.img/${imageName}.webp`;
+    let imagePath = `img/${imageName}.webp`;
 
     imgElement.onerror = function () {
         console.warn(`No se encontró ${imagePath}, probando con .png`);
-        imgElement.src = `.img/${imageName}.png`;
+        imgElement.src = `img/${imageName}.png`;
         imgElement.onerror = function () {
             console.error(`No se encontró la imagen en .webp ni en .png. Usando imagen de respaldo.`);
-            imgElement.src = ".img/default.png";
+            imgElement.src = "img/default.png";
         };
     };
     imgElement.src = imagePath;
